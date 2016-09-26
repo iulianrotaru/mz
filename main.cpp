b9ac3744773d6b2b85be2028d054683e1d197942
iulianrotaru <iulianrotaru59@yahoo.com>
2016-09-26 20:57:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,40 @@
-#include <fstream>
-#define Xp 1000000000
+#include <bits/stdc++.h>
 using namespace std;
-ifstream f("recurenta.in");
-ofstream g("recurenta.out");
-int n,k,i,v[10001][400];
-void add(int C[],int A[],int B[])
-{
-      int i,t=0;
-      for(i=1;i<=A[0]||i<=B[0]||t;i++,t/=Xp)
-      {
-          t+=A[i]+B[i];
-          C[i]=(t>=Xp?t-Xp:t);
-      }
-      C[0]=i-1;
-}
-void scad(int A[],int B[])
-{
-      int i,t=0;
-      for(i=1;i<=A[0];i++)
-      {
-          A[i]-=((i<=B[0])?B[i]:0)+t;
-          A[i]+=(t=A[i]<0)*Xp;
-      }
-      for(;A[0]>1&&!A[A[0]];A[0]--);
-}
+ifstream f("baloane.in");
+ofstream g("baloane.out");
+int n,m,t,i,j,k,v[20];
+double dp[52][52],P[52][15];
 int main()
 {
-    f>>n>>k;
-    for(i=0;i<k;++i) v[i][0]=v[i][1]=1;
-    v[k][0]=1;
-    v[k][1]=k;
-    for(i=k+1;i<=n;++i)
+    f>>t;
+    while(t--)
     {
-        add(v[i],v[i-1],v[i-1]);
-        scad(v[i],v[i-k-1]);
-    }
-    g<<v[n][v[n][0]];
-    for(i=v[n][0]-1;i;--i)
-    {
-        int x=Xp/10;
-        while(v[n][i]<x) g<<0,x/=10;
-        g<<v[n][i];
+        f>>n>>m;
+        for(i=1;i<=m;++i) f>>v[i];
+        for(i=1;i<=n;++i)
+            for(j=1;j<=m;++j)
+            {
+                f>>P[i][j];
+                P[i][j]/=100.0;
+            }
+        double sol=1;
+        for(i=1;i<=m;++i)
+        {
+            for(j=1;j<=n;++j)
+                for(k=1;k<=n;++k) dp[j][k]=0;
+            dp[1][0]=1.0-P[1][i];
+            dp[1][1]=P[1][i];
+            for(j=2;j<=n;++j)
+            {
+                dp[j][0]=dp[j-1][0]*(1.0-P[j][i]);
+                for(k=1;k<=j;++k)
+                    dp[j][k]=dp[j-1][k]*(1.0-P[j][i])+dp[j-1][k-1]*P[j][i];
+            }
+            double s=0;
+            for(j=0;j<=v[i];++j) s+=dp[n][j];
+            sol*=s;
+        }
+        g<<fixed<<setprecision(4)<<sol*100.0<<'\n';
     }
     return 0;
 }
