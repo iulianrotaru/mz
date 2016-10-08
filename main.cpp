f85080ecf5cc9c2892e201887d51cc373a58ebab
iulianrotaru <iulianrotaru59@yahoo.com>
2016-10-08 12:40:59 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,40 +1,59 @@
-#include <bits/stdc++.h>
+#include <fstream>
 using namespace std;
-ifstream f("baloane.in");
-ofstream g("baloane.out");
-int n,m,t,i,j,k,v[20];
-double dp[52][52],P[52][15];
+ifstream f("bitonic.in");
+ofstream g("bitonic.out");
+int n,t,sol,i,poz,a[1<<10],B[1<<10],D[1<<10],S[1<<10],T[1<<10];
+int search_bin(int val)
+{
+    int j,step;
+    for(j=0,step=1<<10;step;step>>=1)
+        if(j+step<=B[0]&&B[j+step]<val) j+=step;
+    return j;
+}
+int search_bin_(int val)
+{
+    int j,step;
+    for(j=0,step=1<<10;step;step>>=1)
+        if(j+step<=D[0]&&D[j+step]>val) j+=step;
+    return j;
+}
 int main()
 {
     f>>t;
     while(t--)
     {
-        f>>n>>m;
-        for(i=1;i<=m;++i) f>>v[i];
-        for(i=1;i<=n;++i)
-            for(j=1;j<=m;++j)
-            {
-                f>>P[i][j];
-                P[i][j]/=100.0;
-            }
-        double sol=1;
-        for(i=1;i<=m;++i)
+        f>>n>>a[1];
+        B[1]=a[1];
+        D[1]=a[1];
+        B[0]=D[0]=1;
+        for(i=2;i<=n;++i)
         {
-            for(j=1;j<=n;++j)
-                for(k=1;k<=n;++k) dp[j][k]=0;
-            dp[1][0]=1.0-P[1][i];
-            dp[1][1]=P[1][i];
-            for(j=2;j<=n;++j)
-            {
-                dp[j][0]=dp[j-1][0]*(1.0-P[j][i]);
-                for(k=1;k<=j;++k)
-                    dp[j][k]=dp[j-1][k]*(1.0-P[j][i])+dp[j-1][k-1]*P[j][i];
-            }
-            double s=0;
-            for(j=0;j<=v[i];++j) s+=dp[n][j];
-            sol*=s;
+            f>>a[i];
+            poz=search_bin(a[i]);
+            if(poz==B[0]) ++B[0];
+            B[poz+1]=a[i];
+            S[i]=poz+1;
+            poz=search_bin_(a[i]);
+            if(poz==D[0]) ++D[0];
+            D[poz+1]=a[i];
+            T[i]=poz+1;
         }
-        g<<fixed<<setprecision(4)<<sol*100.0<<'\n';
+        sol=max(S[n],T[n]);
+        B[1]=a[n];
+        D[1]=a[n];
+        B[0]=D[0]=1;
+        for(i=n-1;i>0;--i)
+        {
+            poz=search_bin(a[i]);
+            if(poz==B[0]) ++B[0];
+            B[poz+1]=a[i];
+            sol=max(sol,S[i]+poz);
+            poz=search_bin_(a[i]);
+            if(poz==D[0]) ++D[0];
+            D[poz+1]=a[i];
+            sol=max(sol,T[i]+poz);
+        }
+        g<<sol<<'\n';
     }
     return 0;
 }
